d90440dbe17e676f6005abdff0e414b53ffc80c7
Kristian Barrett <kba@firstagenda.com>
2019-07-16 23:15:58 +0200

    Added setup for the API and added coreinstaller

--- src/PaymentGatewayAPI/Startup.cs
+++ src/PaymentGatewayAPI/Startup.cs
@@ -1,31 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Internal;
+using PaymentGatewayCore;
+using PaymentGatewayDatabase;
 
 namespace PaymentGatewayAPI
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
         public void ConfigureServices(IServiceCollection services)
         {
-        }
+            services.AddSingleton<ISystemClock, SystemClock>();
+            //AutoMapper configs
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            Mapper.Initialize(x => x.AddMaps(AppDomain.CurrentDomain.GetAssemblies()));
+            Mapper.Configuration.AssertConfigurationIsValid();
+            //END AutoMapper configs
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            services
+                .AddCoreServices(Configuration["ConnectionString"])
+                .AddMvc();
+        }
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
-            app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
+            UpdateDatabase(app);
+            app.UseMvc();
+        }
+        
+        /// <summary>
+        /// Only used as a part of showcase.
+        ///
+        /// Normally the database migrations will be run as a part of the CI/CD pipe
+        /// and not as part of the service startup 
+        /// </summary>
+        private static void UpdateDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = 
+                app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<DatabaseContext>())
+                {
+                    context.Database.Migrate();
+                }
+            }
         }
     }
 }
---
+++ src/PaymentGatewayCore/CoreInstaller.cs
@@ -0,0 +1,22 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PaymentGatewayCore.Repository;
+using PaymentGatewayDatabase;
+
+namespace PaymentGatewayCore
+{
+    public static class CoreInstaller
+    {
+        public static IServiceCollection AddCoreServices(this IServiceCollection services, string connectionString)
+        {
+            services.AddEntityFrameworkSqlServer()
+                .AddDbContext<DatabaseContext>(o =>
+                    o.UseSqlServer(connectionString), ServiceLifetime.Transient)
+                .BuildServiceProvider();
+            services.AddMediatR(typeof(CoreInstaller).Assembly);
+            services.AddTransient<IPaymentRepository, PaymentRepository>();
+            return services;
+        }
+    }
+}
